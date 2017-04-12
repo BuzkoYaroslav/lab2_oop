e9821bb00b590be6cc2c7c8f0b68ed0c96be43de
BuzkoYaroslav <yarik10.98@gmail.com>
2017-04-12 22:40:01 +0300

    create stack class

--- Lab2_oop/Lab2_oop/Stack.h
+++ Lab2_oop/Lab2_oop/Stack.h
@@ -1,7 +1,20 @@
 #pragma once
-class Stack
-{
+#include "PushPopContainer.h"
+#include "DataNode.h"
+
+class Stack: public PushPopContainer {
+private:
+	DataNode *head;
+	int numberOfElements;
 public:
+	int size() const;
+	bool isEmpty() const;
+	char * toString() const;
+
+	int peek() const;
+	int pop();
+	bool push(int);
+
 	Stack();
 	~Stack();
 };
